0afbc8e8cd8058053f97b3bf78d015845b59e25c
durn99 <Bb711081@gmail.com>
2026-09-17 00:17:19 -0600

    Worked On State Machine
    Also added 2 frame idle animation.

--- scripts/arena/character.gd
+++ scripts/arena/character.gd
@@ -1,25 +1,14 @@
+class_name Player
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@onready var state_machine: StateMachine = $"State Machine"
+@onready var animation: AnimationPlayer = $Animation
 
+func _ready(): state_machine.init()
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+func _process(delta): state_machine.process_frame(delta)
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+func _physics_process(delta): state_machine.process_physics(delta)
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+func _input(event): state_machine.process_input(event)
